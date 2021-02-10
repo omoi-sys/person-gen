1f7282f3ed7e982ef00a7039ce6dfb6b10aa7fc5
Abraham <serratab@oregonstate.edu>
2021-02-10 14:01:09 -0800

    added faster loading and random line number selection

--- person-generator.py
+++ person-generator.py
@@ -1,14 +1,55 @@
 import tkinter
 import random
+import sys
+
+num_to_generate = 10
+input_argument = ""
+
+# name of inputfile in case of input csv file passed as an argument when initiating program
+if (len(sys.argv) > 1):
+    input_argument = sys.argv[1]
+
+data_lengths = {
+    "ak": 12,
+    "az": 12,
+    "ca": 1222,
+    "co": 12,
+    "hi": 12,
+    "id": 12,
+    "mt": 12,
+    "nm": 12,
+    "nv": 12,
+    "or": 12,
+    "ut": 12,
+    "wa": 12,
+    "wy": 12
+}
+
+line_nums = []
+
+random.seed()
+
+for i in range(10):
+    line_nums.append(random.randrange(1, data_lengths["ca"]))
+
+line_nums.sort()
+print(line_nums)
 
 address_lines = []
 
 file = open("addresses/ca.csv")
 for i, line in enumerate(file):
-    address_lines.append(line)
+    if i == line_nums[0]:
+        address_lines.append(line)
+        line_nums.pop(0)
+    if len(line_nums) == 0:
+        break
 
-print(address_lines[134544].split(','))
-new_one = address_lines[134544].split(',')
+file.close()
 
-print(new_one[2] + " " + new_one[3] + " " + new_one[4] + ", " + new_one[5] + " " + new_one[8])
+for i in range(0, len(address_lines)):
+    print(address_lines[i].split(','))
+    new_one = address_lines[i].split(',')
+    print(new_one[2] + " " + new_one[3] + " " + new_one[4] + ", " + new_one[5] + " " + new_one[8])
+
 print("Done")
